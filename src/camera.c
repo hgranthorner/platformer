a1329e880f13462e6e74584e877f18c6c21939fc
Grant Horner <h.grant.horner@gmail.com>
2023-01-31 23:22:48 -0500

    Add vertical camera movement

--- src/camera.c
+++ src/camera.c
@@ -10,4 +10,10 @@ void move_camera(const Player *player, SDL_Rect *camera)
 
   if (r.x < camera->x + camera->w * 1/4)
     camera->x = r.x - camera->w * 1/4;
+
+  if (r.y > camera->y + camera->h * 7/8)
+    camera->y = r.y - camera->h * 7/8;
+
+  if (r.y < camera->y + camera->h * 1/8)
+    camera->y = r.y - camera->h * 1/8;
 }
