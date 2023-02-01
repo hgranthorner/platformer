aa85930972b3aa4c9c48a1e450c4064522653c71
Grant Horner <h.grant.horner@gmail.com>
2023-01-31 23:19:24 -0500

    Add padding to camera

--- src/camera.c
+++ src/camera.c
@@ -5,9 +5,9 @@
 void move_camera(const Player *player, SDL_Rect *camera)
 {
   SDL_Rect r = player->rect.shape;
-  if (r.x > camera->x + camera->w * 4/5)
-    camera->x = r.x - camera->w * 4/5;
+  if (r.x > camera->x + camera->w * 3/4)
+    camera->x = r.x - camera->w * 3/4;
 
-  if (r.x < camera->x + camera->w * 1/5)
-    camera->x = r.x - camera->w * 1/5;
+  if (r.x < camera->x + camera->w * 1/4)
+    camera->x = r.x - camera->w * 1/4;
 }
